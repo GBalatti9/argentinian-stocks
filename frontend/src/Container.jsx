bbea9a4eda7d62cdf1cb47fd660435267b76b923
GBalatti9 <gas.balatti@gmail.com>
2023-11-30 08:42:11 -0300

    Styling frontend

--- frontend/src/Container.jsx
+++ frontend/src/Container.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { LoadingSpinner } from "./components/LoadingSpinner";
 import { Table } from "./components/Table";
 import { useFetch } from "./hooks/useFetch"
@@ -5,6 +6,18 @@ import { useFetch } from "./hooks/useFetch"
 export const Container = () => {
     const { info } = useFetch();
     const { data, lastActualization } = info;
+
+    const [showMessage, setShowMessage] = useState(false);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (data.length === 0) {
+                setShowMessage(true);
+            }
+        }, 15000);
+
+        return () => clearTimeout(timeoutId);
+    }, [data]);
 
     const bullish = data.slice(0, 5);
     const bearish = data.slice(5);
@@ -29,26 +42,43 @@ export const Container = () => {
     return (
         <>
             {
-                data.length === 0 ?
-                    <LoadingSpinner /> :
+                data.length === 0 && !showMessage ? (
+                    <LoadingSpinner />
+                ) :
                     <div className="d-flex flex-column justify-content-center" style={{
                         height: '100vh',
                     }}>
-                        <video autoPlay loop muted style={{ objectFit: 'cover', width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, zIndex: -1 }}>
-                            <source src="/spaceship.mp4" type="video/mp4" />
-                            Tu navegador no soporta el video.
-                        </video>
-                        <p className="text-center"> Última actualización: {lastActualization} </p>
+                        {
+                            showMessage ? (
+                                <h3 className="text-center">
+                                    The stock market is closed :(
+                                    <br />
+                                    <span>Bearish...</span>
+                                </h3>
+                            )
+                                : (
+                                    <>
+                                        <video autoPlay loop muted style={{ objectFit: 'cover', width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, zIndex: -1 }}>
+                                            <source src="/spaceship.mp4" type="video/mp4" />
+                                            Your browser does not support the video.
+                                        </video>
+                                        <p className="text-center"> 
+                                            Última actualización: 
+                                            { lastActualization } 
+                                        </p>
 
-                        <div className="d-flex flex-column justify-content-around align-items-center d-md-none">
-                            <Table stonksReceived={bullishStonks} />
-                            <Table stonksReceived={bearishStonks} />
-                        </div>
+                                        <div className="d-flex flex-column justify-content-around align-items-center d-md-none">
+                                            <Table stonksReceived = { bullishStonks } />
+                                            <Table stonksReceived = { bearishStonks } />
+                                        </div>
 
-                        <div className="d-none d-md-flex justify-content-around align-items-center">
-                            <Table stonksReceived={bullishStonks} />
-                            <Table stonksReceived={bearishStonks} />
-                        </div>
+                                        <div className="d-none d-md-flex justify-content-around align-items-center">
+                                            <Table stonksReceived = { bullishStonks } />
+                                            <Table stonksReceived = { bearishStonks } />
+                                        </div>
+                                    </>
+                                )
+                        }
                     </div>
             }
         </>
