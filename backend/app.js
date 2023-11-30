60140955846c582b2ab572abcbe6b492a532a4a2
GBalatti9 <gas.balatti@gmail.com>
2023-11-30 09:01:01 -0300

    Modify backend port and package.json to deploy

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,13 @@ const { scrappWeb } = require('./helpers/webScrapping');
 const app = express();
 
 app.use(cors());
-const PORT = 3000;
+const port = 3000;
 
 app.get('/web-scrapping-api', async (req, res) => {
     const data  = await scrappWeb();
     res.json({ data });
 })
 
-app.listen(PORT, () => {
-    console.log(`App listening on ${PORT}...`);
+app.listen( process.env.PORT || port, () => {
+    console.log(`App listening on ${port}...`);
 })
