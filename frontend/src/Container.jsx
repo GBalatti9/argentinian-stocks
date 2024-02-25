689e8ce2f8037ec0d17714a7f0e60fa0eb8debd1
GBalatti9 <gas.balatti@gmail.com>
2024-02-25 10:45:59 -0300

    Responsive

--- frontend/src/Container.jsx
+++ frontend/src/Container.jsx
@@ -1,23 +1,11 @@
 import { LoadingSpinner } from "./components/LoadingSpinner";
 import { Table } from "./components/Table";
-import { useFetch } from "./hooks/useFetch";
 import { useFetchHook } from "./hooks/useFetchHook";
 
 export const Container = () => {
-    const { info } = useFetch();
     const { stocksData } = useFetchHook();
     const { loading, data, lastActualizacion } = stocksData;
-    console.log(lastActualizacion);
 
-    // const { data, lastActualization, isLoading } = info;
-
-    // let bullish;
-    // let bearish;
-    // if (data.length > 1) {
-    //     bullish = data.slice(0, 5);
-    //     bearish = data.slice(5);
-    // }
-    // // const columnTitles = ["Ticker", "Último precio", "Var. Diaria (%)"];
     const columnTitles = ["Ticker", "Último precio", "Var. Diaria (%)"];
 
     const bullishStonks = {
@@ -59,10 +47,10 @@ export const Container = () => {
                             Última vez: { lastActualizacion }
                         </p>
 
-                        {/* <div className="d-flex flex-column justify-content-around align-items-center d-md-none">
+                        <div className="d-flex flex-column justify-content-around align-items-center d-md-none">
                             <Table {...bullishStonks} />
                             <Table {...bearishStonks} />
-                        </div> */}
+                        </div>
 
                         <div className="d-none d-md-flex justify-content-around align-items-center">
                             <Table {...bullishStonks} />
--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,79 +1,79 @@
-import { useEffect, useState } from "react";
-import { format } from 'date-fns';
-import { es } from 'date-fns/locale';
+// import { useEffect, useState } from "react";
+// import { format } from 'date-fns';
+// import { es } from 'date-fns/locale';
 
-// Using this stocks when playwright doesnt work
-import stocks from '../data/scrapedData.json';
+// // Using this stocks when playwright doesnt work
+// import stocks from '../data/scrapedData.json';
 
-const URL = 'http://localhost:3000/web-scrapping-api';
-// const URL = 'https://argentinian-stocks-backend.onrender.com/web-scrapping-api';
+// const URL = 'http://localhost:3000/web-scrapping-api';
+// // const URL = 'https://argentinian-stocks-backend.onrender.com/web-scrapping-api';
 
-export const useFetch = () => {
-    const [ info, setInfo ] = useState({
-        isLoading: true,
-        lastActualization: '',
-        data: []
-    });
+// export const useFetch = () => {
+//     const [ info, setInfo ] = useState({
+//         isLoading: true,
+//         lastActualization: '',
+//         data: []
+//     });
 
-    const fetchApi = async (url) => {
-        try {
-            setInfo(( prevInfo ) => ({
-                ...prevInfo,
-                isLoading: true,
-            }))
+//     const fetchApi = async (url) => {
+//         try {
+//             setInfo(( prevInfo ) => ({
+//                 ...prevInfo,
+//                 isLoading: true,
+//             }))
 
-            const response = await fetch(url);
-            if (!response.ok) {
-                // If somehow the fetch response doesn't works then the stocks shown are gonna be the imported from '../data/scrapedData.json';
-                setInfo(( prevInfo ) => ({
-                    ...prevInfo,
-                    isLoading: false,
-                    lastActualization: format(new Date(), 'EEEE d MMM yyyy', { locale: es }),
-                    data: stocks,
-                }))
-                // console.error("USANDO STOCKS");
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const { data } = await response.json();
-            // console.log( data );
-            let dataFromJson;
+//             const response = await fetch(url);
+//             if (!response.ok) {
+//                 // If somehow the fetch response doesn't works then the stocks shown are gonna be the imported from '../data/scrapedData.json';
+//                 setInfo(( prevInfo ) => ({
+//                     ...prevInfo,
+//                     isLoading: false,
+//                     lastActualization: format(new Date(), 'EEEE d MMM yyyy', { locale: es }),
+//                     data: stocks,
+//                 }))
+//                 // console.error("USANDO STOCKS");
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
+//             const { data } = await response.json();
+//             // console.log( data );
+//             let dataFromJson;
 
-            // When the market is close data is undefined so to prevent an error the info is gonna be taken from stocks.json.
-            if ( data === undefined ) {
-                dataFromJson = stocks;
-            }
+//             // When the market is close data is undefined so to prevent an error the info is gonna be taken from stocks.json.
+//             if ( data === undefined ) {
+//                 dataFromJson = stocks;
+//             }
 
-            setInfo(( prevInfo ) => ({
-                ...prevInfo,
-                isLoading: false,
-                lastActualization: format(new Date(), 'EEEE d MMM yyyy', { locale: es }),
-                data: data === undefined ? dataFromJson : data,
-            }))
-        } catch (error) {
-            // If somehow fetch doesn't works then the stocks shown are gonna be the imported from '../data/scrapedData.json';
-            // console.error('Error fetching data: ', error);
-            setInfo(( prevInfo ) => ({
-                ...prevInfo,
-                isLoading: false,
-                lastActualization: format(new Date(), 'EEEE d MMM yyyy', { locale: es }),
-                data: stocks,
-            }))
-        }
-    };
+//             setInfo(( prevInfo ) => ({
+//                 ...prevInfo,
+//                 isLoading: false,
+//                 lastActualization: format(new Date(), 'EEEE d MMM yyyy', { locale: es }),
+//                 data: data === undefined ? dataFromJson : data,
+//             }))
+//         } catch (error) {
+//             // If somehow fetch doesn't works then the stocks shown are gonna be the imported from '../data/scrapedData.json';
+//             // console.error('Error fetching data: ', error);
+//             setInfo(( prevInfo ) => ({
+//                 ...prevInfo,
+//                 isLoading: false,
+//                 lastActualization: format(new Date(), 'EEEE d MMM yyyy', { locale: es }),
+//                 data: stocks,
+//             }))
+//         }
+//     };
 
-    useEffect(() => {
-            fetchApi(URL);
+//     useEffect(() => {
+//             fetchApi(URL);
 
-            const intervalId = setInterval(() => {
-                fetchApi(URL);
-            }, 60000); 
+//             const intervalId = setInterval(() => {
+//                 fetchApi(URL);
+//             }, 60000); 
             
-            return () => clearInterval(intervalId);
-    }, [])
-    // console.log(new Date().toString().split(' GMT')[0]);
+//             return () => clearInterval(intervalId);
+//     }, [])
+//     // console.log(new Date().toString().split(' GMT')[0]);
 
-    return {
-        info,
-        setInfo,
-    }
-}
+//     return {
+//         info,
+//         setInfo,
+//     }
+// }
