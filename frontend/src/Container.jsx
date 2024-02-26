256504ad4a298fd3c567e7e14524a59992f1a0da
GBalatti9 <gas.balatti@gmail.com>
2024-02-26 12:12:25 -0300

    Changing background and searching different table styles

--- frontend/src/Container.jsx
+++ frontend/src/Container.jsx
@@ -29,32 +29,28 @@ export const Container = () => {
             {
                 loading && data.length === 0 ?
                 (
-                    <LoadingSpinner />
+                    <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+                        <LoadingSpinner />
+                    </div>
                 ) : 
                 (
-                    <div style={{ 
-                            height: '100vh', 
-                            display: 'flex', 
-                            flexDirection: 'column',
-                            justifyContent: 'center' }}>
-                        <video autoPlay loop muted style={{ objectFit: 'cover', width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, zIndex: -1 }}>
-                            <source src="/spaceship.mp4" type="video/mp4" />
-                            Your browser does not support the video.
-                        </video>
-                        <p className="text-center">
-                            La información se actualiza cada 20 minutos.
-                            <br />
-                            Última vez: { lastActualizacion }
-                        </p>
+                    <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+                        <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                            <p className="text-center">
+                                La información se actualiza cada 20 minutos.
+                                <br />
+                                Última vez: { lastActualizacion }
+                            </p>
 
-                        <div className="d-flex flex-column justify-content-around align-items-center d-md-none">
-                            <Table {...bullishStonks} />
-                            <Table {...bearishStonks} />
-                        </div>
+                            <div className="d-flex flex-column justify-content-around align-items-center d-md-none">
+                                <Table {...bullishStonks} />
+                                <Table {...bearishStonks} />
+                            </div>
 
-                        <div className="d-none d-md-flex justify-content-around align-items-center">
-                            <Table {...bullishStonks} />
-                            <Table {...bearishStonks} />
+                            <div className="d-none d-md-flex justify-content-around align-items-center">
+                                <Table {...bullishStonks} />
+                                <Table {...bearishStonks} />
+                            </div>
                         </div>
                     </div>
                 )
@@ -62,3 +58,13 @@ export const Container = () => {
         </>
     )
 }
+
+// <div style={{ 
+                    //         height: '100vh', 
+                    //         display: 'flex', 
+                    //         flexDirection: 'column',
+                    //         justifyContent: 'center' }}>
+                    //     <video autoPlay loop muted style={{ objectFit: 'cover', width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, zIndex: -1 }}>
+                    //         <source src="/spaceship.mp4" type="video/mp4" />
+                    //         Your browser does not support the video.
+                    //     </video>
--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -1,9 +1,11 @@
 
 export const LoadingSpinner = () => {
     return (
-        <div className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-            <strong>Bullish is coming...</strong>
-            <div className="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+        <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <div>
+                <strong>Bullish is coming...</strong>
+                <div className="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+            </div>
         </div>
     )
 }
